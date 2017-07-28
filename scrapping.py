ee1eaa61d2b827eb7823b21b896565741e6af15b
kemalcanbora <kemalcanbora@gmail.com>
2017-07-28 14:20:50 +0300

    selenium scrp for twitter

---
+++ scrapping.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from getpass import getpass
+
+
+def login_twitter(username, password):
+    driver = webdriver.Chrome("/usr/bin/chromedriver")
+    driver.get("https://twitter.com/login")
+
+    username_field = driver.find_element_by_class_name("js-username-field")
+    password_field = driver.find_element_by_class_name("js-password-field")
+
+    username_field.send_keys(username)
+    driver.implicitly_wait(1)
+
+    password_field.send_keys(password)
+    driver.implicitly_wait(1)
+
+    driver.find_element_by_class_name("EdgeButtom--medium").click()
+
+
+if __name__ == "__main__":
+    username = input("user name : ")
+    password = getpass("password  : ")
+    login_twitter(username, password)
