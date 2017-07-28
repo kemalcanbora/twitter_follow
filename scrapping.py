bb1744647f94e7e4aa4e5c19e759fe736ee5f207
kemalcanbora <kemalcanbora@gmail.com>
2017-07-28 17:59:12 +0300

    chromedriver

--- scrapping.py
+++ scrapping.py
@@ -19,22 +19,29 @@ def login_twitter(username, password):
 
     driver.find_element_by_class_name("EdgeButtom--medium").click()
 
-    url = 'https://twitter.com/workudy/followers'
+    url = 'https://twitter.com/kemalcanbora/following'
 
     driver.get(url)
+
     initial_value = 0
-    end = 30
+
+
+    end = 30000
     for i in range(1000, end, 1000):
         driver.execute_script("window.scrollTo(" + str(initial_value) + ', ' + str(i) + ")")
         time.sleep(0.5)
         initial_value = i
+        liste = driver.find_elements(By.XPATH,"//b[@class='u-linkComplex-target']")
+    for el in liste:
+        print(el.text)
 
-    el = driver.find_element(By.XPATH, "//div[@class='u-textTruncate u-inlineBlock ProfileNameTruncated-withBadges ProfileNameTruncated-withBadges--1']")
-    print(el.text)
+
+    # el = driver.find_element(By.XPATH, "//div[@class='u-textTruncate u-inlineBlock ProfileNameTruncated-withBadges ProfileNameTruncated-withBadges--1']")
+    # print(el.text)
 
     time.sleep(600)
 
-username = "usernmae"#input("user name : ")
+username = "tuulrik"#input("user name : ")
 password = "pass"#getpass("password  : ")
 login_twitter(username, password)
 
